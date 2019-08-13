84e615a3599c3b4fab727518244c1f587b3da04a
Finn Walker <finn3walker@gmail.com>
2019-08-13 04:41:36 +0100

    Added static folder and prep SPA

--- server/index.js
+++ server/index.js
@@ -11,5 +11,10 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
+app.use(express.static(__dirname + '/public'));
+app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
